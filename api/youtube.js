f43e5e1b77ee488dde6552af7ed5711f13ec205c
gabrielbzago <gabrielbzago@gmail.com>
2026-08-07 13:07:33 -0300

    Add files via upload

--- api/youtube.js
+++ api/youtube.js
@@ -1746,7 +1746,7 @@ const coverageDifficulty = Math.round(
 // -------------------------
 // Competição Final
 // -------------------------
-competition = Math.max(
+const competition = Math.max(
 
     competitionMarketDifficulty,
 
